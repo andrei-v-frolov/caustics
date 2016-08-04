36c53cf49cba47b7cef426f561c862ed1edaa734
Andrei Frolov <frolov@sfu.ca>
2016-08-04 16:27:47 -0700

    PDF data is also loaded.

--- plot.py
+++ plot.py
@@ -8,6 +8,7 @@ import numpy as np
 import scipy.stats
 from pylab import *
 
+# load trajectory data
 hdu = pyfits.open('smpout.fit')
 
 phi = hdu[0].data
@@ -17,6 +18,17 @@ pic = hdu[3].data
 a   = hdu[4].data
 p   = hdu[5].data
 
+# load PDF data, J = P(t)/P(0)
+pdf = pyfits.open('derivs.fit')
+
+J_phi = pdf[0].data
+J_chi = pdf[1].data
+J_pip = pdf[2].data
+J_pic = pdf[3].data
+J_a   = pdf[4].data
+J_p   = pdf[5].data
+
+# variable being plotted
 q = -p/6.0
 
 np.putmask(q, np.isnan(q), 0.0)
@@ -24,6 +36,7 @@ np.putmask(q, np.isinf(q), 0.0)
 
 ny,nx = q.shape
 
+# data extent and meshes
 x0 = hdu[0].header['CRVAL1']
 y0 = hdu[0].header['CRVAL2']
 dx = hdu[0].header['CDELT1']
@@ -34,7 +47,7 @@ y = y0 + arange(ny)*dy
 
 X, Y = meshgrid(x, y)
 
-hdu.close()
+hdu.close(); pdf.close()
 
 ## destripe in time direction
 for i in range(0,ny):
@@ -65,6 +78,7 @@ imshow(Q, extent=(x[0],x[-1],y[0],y[-1]), origin='lower', aspect=dx/dy, cmap=c, 
 data3d = np.load('data/slice-1875.npz')
 plot(data3d['x'], 50.0+data3d['dlna'], 'k-')
 
+xlim([x[0],x[-1]])
 #xticks(arange(5)-11.0)
 
 xlabel('$\\alpha \equiv \ln(\chi_0/\phi_0)/\mu_0T$')
